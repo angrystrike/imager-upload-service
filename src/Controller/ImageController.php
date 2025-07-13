6d855745253c29c6e4339c3752dc6fa955ff4286
Maksym Melnyk <melnik.max00@gmail.com>
2025-07-13 18:20:27 +0000

    Debug

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -21,6 +21,7 @@ class ImageController extends AbstractController
     #[Route('/images', methods: ['GET'])]
     public function images(CacheInterface $cache): JsonResponse
     {
+        return new JsonResponse([], JsonResponse::HTTP_OK, [], true);
         $cacheKey = 'all_images_json';
 
         $images = $cache->get($cacheKey, function (ItemInterface $item) {
